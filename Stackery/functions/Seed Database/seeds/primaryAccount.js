cb5ac4852df4ae2c20bcb451b5674a99f220bb78
Toby Fee <TobyFee@gmail.com>
2018-07-17 13:48:09 -0700

    new seeds

--- Stackery/functions/Seed Database/seeds/primaryAccount.js
+++ Stackery/functions/Seed Database/seeds/primaryAccount.js
@@ -1,2 +1,5 @@
 // knex.js doesn't do upserts natively, so use raw INSERT IGNORE query string
-exports.seed = knex => knex.raw("INSERT IGNORE INTO accounts (name) VALUES ('Stackery')");
+exports.seed = knex => {
+    knex.raw("INSERT IGNORE INTO accounts (name,id) VALUES ('Stackery',37)");
+    knex.raw("INSERT IGNORE INTO accounts (name,id) VALUES ('AlutiraLTD',47)");
+}
